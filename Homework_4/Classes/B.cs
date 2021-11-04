b22de27189f3a683f4e7e099e412e805400c7198
SashkaShashka <sasha151299qwe@gmail.com>
2021-11-04 11:25:04 +0400

    Добавьте файлы проекта.

---
+++ Homework_4/Classes/B.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Homework_4;
+
+namespace Homework_4
+{
+    class B : A
+    {
+
+    }
+}
---
+++ Homework_4/Classes/А.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_4
+{
+    public abstract class А
+    {
+        public string Prop1 = "aaa";
+        public int Prop2
+        {
+            get => Prop2;
+            set
+            {
+                if (value <= 100 && value >= 0)
+                    Prop2 = value;
+            }
+        }
+        public int Foo()
+        {
+            return 0;
+        }
+        public void Foo(DateTime dateTime)
+        {
+
+        }
+    }
+}
